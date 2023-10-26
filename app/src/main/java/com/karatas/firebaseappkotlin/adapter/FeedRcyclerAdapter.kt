deff2bdba4384e232dadc50d0dfa5a53de686235
Vahdet Karatas <38797434+vahdetkaratas@users.noreply.github.com>
2023-10-26 19:13:05 +0300

    first commit
    Working with Firebase

---
+++ app/src/main/java/com/karatas/firebaseappkotlin/adapter/FeedRcyclerAdapter.kt
@@ -0,0 +1,36 @@
+package com.karatas.firebaseappkotlin.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.karatas.firebaseappkotlin.databinding.RecyclerViewRowBinding
+import com.karatas.firebaseappkotlin.model.Post
+import com.squareup.picasso.Picasso
+
+class FeedRecyclerAdapter(private val postList : ArrayList<Post>) : RecyclerView.Adapter<FeedRecyclerAdapter.PostHolder>() {
+
+    class PostHolder(val binding: RecyclerViewRowBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val binding = RecyclerViewRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return PostHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+        holder.binding.recyclerEmailText.text = postList.get(position).email
+        holder.binding.recyclerCommentText.text = postList.get(position).comment
+        Picasso.get().load(postList[position].downloadUrl).into(holder.binding.recyclerImageView)
+    }
+
+
+}
